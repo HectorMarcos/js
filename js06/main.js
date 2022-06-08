a06b39caee9253d860a88a778d5a6757bd6f68a7
Hector Marcos <hectormarcx@gmail.com>
2022-06-08 17:55:19 +0200

    js06

---
+++ js06/main.js
@@ -0,0 +1,24 @@
+////
+
+let sum = (i, j) => {
+	k = i + j;
+	return k;
+};
+
+console.log(sum(3, 2));
+
+////
+
+let miName = (noname, surname1, surname2) => {
+	console.log(noname + " " + surname1 + " " + surname2);
+};
+
+let showBigger = (i, j) => {
+	if (i > j) {
+		console.log(i + " is the bigger one");
+	} else if (i < j) {
+		console.log(j + " is the bigger one");
+	} else if ((i = j)) {
+		console.log("Both numbers are the same");
+	}
+};

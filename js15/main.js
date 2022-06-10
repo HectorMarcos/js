12d31352ec51195619fbaa877efd97677f3c48e3
Hector Marcos <hectormarcx@gmail.com>
2022-06-10 09:33:49 +0200

    js15

--- js15/main.js
+++ js15/main.js
@@ -1,10 +1,18 @@
 let solveQuestion = (event) => {
 	let question = event.currentTarget.getAttribute("id");
+	//console.log(question);
 	let answer = "d";
 	if (question === answer) {
-		event.target.style.backgroundColor = "green";
+		//event.target.style.backgroundColor = "green";
+		//event.className += " success";
+		//question.classList.add("success");
 		//event.target.classList.add("success");
-		//console.log(event);
-	} else event.target.style.backgroundColor = "red";
-	//event.target.classList.add("fail");
+		//event.classList.add("success");
+		//console.log(event.srcElement.id);
+		const element = document.getElementById(event.srcElement.id);
+		element.classList.add("success");
+	} else {
+		const element = document.getElementById(event.srcElement.id);
+		element.classList.add("fail");
+	}
 };
